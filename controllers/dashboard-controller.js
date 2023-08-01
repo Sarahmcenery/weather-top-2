62a7ce6b782ddb4990df9eaeb0809284a39ea012
Glitch (weather-top2) <none>
2023-08-01 18:31:54 +0000

    Members signup/login

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -1,19 +1,24 @@
 import { stationStore } from "../models/station-store.js";
+import { accountsController } from "./accounts-controller.js";
+
 export const dashboardController = {
   async index(request, response) {
+    const loggedInUser = await accountsController.getLoggedInUser(request);
     const viewData = {
       title: "WeatherTop Dashboard",
-      stations: await stationStore.getAllStations(),
+      stations: await stationStore.getStationsByUserId(loggedInUser._id),
     };
     console.log("dashboardrendering");
     response.render("dashboard-view", viewData);
   },
 
   async addStation(request, response) {
+    const loggedInUser = await accountsController.getLoggedInUser(request);
     const newStation = {
       title: request.body.title,
       latitude: Number(request.body.latitude),
       longitude: Number(request.body.longitude),
+      userid: loggedInUser._id,
     };
     console.log(`adding station ${newStation.title}`);
     await stationStore.addStation(newStation);
--- routes.js
+++ routes.js
@@ -1,16 +1,24 @@
 import express from "express";
-import { indexController } from "./controllers/index-controller.js";
+import { accountsController } from "./controllers/accounts-controller.js";
+//import { indexController } from "./controllers/index-controller.js";
 import { dashboardController } from "./controllers/dashboard-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
 import { stationController } from "./controllers/station-controller.js";
+//import { readingController } from "./controllers/reading-controller.js";
+
 export const router = express.Router();
-//const accounts = require('./controllers/accounts.js');
 
+router.get("/", accountsController.index);
+router.get("/login", accountsController.login);
+router.get("/signup", accountsController.signup);
+router.get("/logout", accountsController.logout);
+router.post("/register", accountsController.register);
+router.post("/authenticate", accountsController.authenticate);
 
-router.get("/", indexController.index);
+//router.get("/", indexController.index);
 router.get("/dashboard", dashboardController.index);
 router.get("/about", aboutController.index);
-router.get("/index", aboutController.index);
+//router.get("/index", aboutController.index);
 
 router.get("/station/:id", stationController.index);
 router.post("/dashboard/addstation", dashboardController.addStation);
